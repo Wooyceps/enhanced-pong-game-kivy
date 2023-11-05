3b2310c75bb68d20e42c44df416258198e6bd4c6
Michał Wójcik <mwooycik@gmail.com>
2023-11-06 00:09:38 +0100

    progress in adding a current exchange and longest exchange widget

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from kivy.clock import Clock
 class PongPaddle(Widget):
     score = NumericProperty(0)
 
-    def bounce_ball(self, ball):
+    def bounce_ball(self, ball, stat_tracker):
         if self.collide_widget(ball):
             vx, vy = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 4)
@@ -17,6 +17,11 @@ class PongPaddle(Widget):
                 print(bounced.x)
                 bounced *= 1.1
             ball.velocity = bounced.x, bounced.y + offset
+
+
+class StatTracker(Widget):
+    current_exchange = NumericProperty(0)
+    best_exchange = NumericProperty(0)
 
 
 class PongBall(Widget):
@@ -32,15 +37,17 @@ class PongGame(Widget):
     ball = ObjectProperty(None)
     player1 = ObjectProperty(None)
     player2 = ObjectProperty(None)
+    stat_tracker = ObjectProperty(None)
 
+    current_exchange = NumericProperty(0)
     def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
         self.ball.velocity = vel
 
     def update(self, dt):
         # bounce off paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
+        self.player1.bounce_ball(self.ball, self.stat_tracker)
+        self.player2.bounce_ball(self.ball, self.stat_tracker)
 
         self.ball.move()
 
